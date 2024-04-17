e601e1593602155bf7dc165a0d4afda673bff8a4
KodyMac <kodymac920024@hotmail.com>
2024-04-17 11:17:32 -0500

    Permissions added

--- src/main/java/model/User.java
+++ src/main/java/model/User.java
@@ -1,5 +1,34 @@
 package model;
 
 public class User {
-
+	private String name;
+	private String email;
+	private PermissionType permissions;
+	
+	public User(String name, String email, PermissionType permissions) {
+		this.name=name;
+		this.email=email;
+		this.permissions=permissions;
+	}
+	
+	
+	
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public String getEmail() {
+		return email;
+	}
+	public void setEmail(String email) {
+		this.email = email;
+	}
+	public PermissionType getPermissions() {
+		return permissions;
+	}
+	public void setPermissions(PermissionType permissions) {
+		this.permissions = permissions;
+	}
 }

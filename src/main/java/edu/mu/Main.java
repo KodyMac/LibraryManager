4bd993f501c383bf65b3917e949ae4c983804415
KodyMac <kodymac920024@hotmail.com>
2024-04-27 21:24:51 -0500

    Restart of main

--- src/main/java/edu/mu/Main.java
+++ src/main/java/edu/mu/Main.java
@@ -1,24 +1,21 @@
 package edu.mu;
 
 import edu.mu.Products.*;
-import edu.mu.Products.ProductFactory;
-import edu.mu.Products.ProductGenre;
-import edu.mu.Products.ProductType;
 import edu.mu.User.*;
 
 public class Main {
 
 	public static void main(String[] args) {
-//		Library library = new Library();
+		Library library = new Library();
 //		//ProductFactory factory = new ProductFactory();
 //		
-//		Customer cust = new Customer("Albert","CustomerEmail@email.com");
-//		Librarian lib = new Librarian("June", "librarianEmail@email.com");
-//		//factory.createProduct("The Complete Saga", "George Lucas", ProductType.CD,ProductGenre.SCI_FI);
-//		//factory.createProduct("Jojo's Bizarre Adventure", "Hirohiko Araki", ProductType.BOOK, ProductGenre.GRAPHIC_NOVEL);
-//		//factory.createProduct("Shonen Jump", "Shuesha", ProductType.MAGAZINE, ProductGenre.POP_CULTURE);
-//		//factory.createProduct("Persona 3 Reload", "Atlus", ProductType.GAME, ProductGenre.ADVENTURE);
-//		library.
+		Customer cust = new Customer("Albert","CustomerEmail@email.com");
+		Librarian lib = new Librarian("June", "librarianEmail@email.com");
+		library.addProductToCatalog(cust, "The Golden Experience", "Prince", ProductType.CD, ProductGenre.POP); //should fail due to customer permission
+		library.addProductToCatalog(lib, "The Golden Experience", "Prince", ProductType.CD, ProductGenre.POP); //should work
+		library.addProductToCatalog(lib, "Jojo's Bizarre Adventure", "Hirohiko Araki", ProductType.BOOK, ProductGenre.GRAPHIC_NOVEL);
+		library.addProductToCatalog(lib, "Shounen Jump", "Shuesha", ProductType.MAGAZINE, ProductGenre.POP_CULTURE);
+		library.addProductToCatalog(lib, "Persona 3 Reload", "Atlus", ProductType.GAME, ProductGenre.ADVENTURE);
 //		/*Generic?*/Book Jojo = factory.createProduct("Jojo's Bizarre Adventure", "Hirohiko Araki", ProductType.BOOK, ProductGenre.ADVENTURE)); 
 //		//library.addProduct(cust, Jojo);//should fail
 //		library.addProduct(lib, factory.createProduct("Jojo's Bizarre Adventure", "Hirohiko Araki", ProductType.BOOK, ProductGenre.ADVENTURE)); //should allow to add

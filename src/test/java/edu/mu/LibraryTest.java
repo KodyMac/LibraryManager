ac64f1929be872557472174efe0cc7dc9531ae93
KodyMac <kodymac920024@hotmail.com>
2024-05-02 13:34:56 -0500

    More tests

--- src/test/java/edu/mu/LibraryTest.java
+++ src/test/java/edu/mu/LibraryTest.java
@@ -19,7 +19,11 @@ class LibraryTest {
 
 	Library library = new Library();
 	ProductFactory fact = new ProductFactory();
-	private Product p = null;
+	private Product b = null;
+	private Product c = null;
+	private Product d = null;
+	private Product g = null;
+	private Product m = null;
 	private Librarian lib = null;
 	private Customer cust = null;
 	
@@ -33,8 +37,16 @@ class LibraryTest {
 
 	@BeforeEach
 	void setUp() throws Exception {
-	p=fact.createProduct("Jojo's", "Araki", ProductType.BOOK, ProductGenre.GRAPHIC_NOVEL);
+	b=fact.createProduct("Jojo's", "Araki", ProductType.BOOK, ProductGenre.GRAPHIC_NOVEL);
+	c=fact.createProduct("Oh, My Heart", "Mother Mother", ProductType.CD, ProductGenre.POP);
+	d=fact.createProduct("Dude, Where's My Car?!", "Danny Leiner", ProductType.DVD, ProductGenre.SCI_FI);
+	g=fact.createProduct("Lego Star Wars", "Travelers Tales", ProductType.GAME, ProductGenre.ADVENTURE);
+	m=fact.createProduct("Tiger Beat", "Laufer Media", ProductType.MAGAZINE, ProductGenre.POP_CULTURE);
 	lib=new Librarian("June", "librarianEmail@email.com");
+//	library.addProductToCatalog(lib, c.getTitle(), c.getAuthor(), c.getType(), c.getGenre());
+//	library.addProductToCatalog(lib, g.getTitle(), g.getAuthor(), g.getType(), g.getGenre());
+//	library.addProductToCatalog(lib, d.getTitle(), d.getAuthor(), d.getType(), d.getGenre());
+//	library.addProductToCatalog(lib, m.getTitle(), m.getAuthor(), m.getType(), m.getGenre());
 	cust = new Customer("Albert","CustomerEmail@email.com");
 	
 	}
@@ -44,13 +56,38 @@ class LibraryTest {
 	}
 
 	@Test
+	void testLibAddProductToCatalog() {
+		assertEquals(true,library.addProductToCatalog(lib, b.getTitle(), b.getAuthor(), b.getType(), b.getGenre()));
+		assertEquals("June",lib.getName());
+		assertEquals("librarianEmail@email.com",lib.getEmail());
+		assertEquals(true,library.addProductToCatalog(lib, c.getTitle(), c.getAuthor(), c.getType(), c.getGenre()));
+		assertEquals(true,library.addProductToCatalog(lib, g.getTitle(), g.getAuthor(), g.getType(), g.getGenre()));
+		assertEquals(true,library.addProductToCatalog(lib, d.getTitle(), d.getAuthor(), d.getType(), d.getGenre()));
+		assertEquals(true,library.addProductToCatalog(lib, m.getTitle(), m.getAuthor(), m.getType(), m.getGenre()));
+	}
+	@Test
 	void testCustAddProductToCatalog() {
-		assertEquals(false,library.addProductToCatalog(cust, p.getTitle(), p.getAuthor(), p.getType(), p.getGenre()));
+		assertEquals(false,library.addProductToCatalog(cust, b.getTitle(), b.getAuthor(), b.getType(), b.getGenre()));
+		assertEquals("Albert",cust.getName());
+		assertEquals("CustomerEmail@email.com",cust.getEmail());
+		assertEquals(false,library.addProductToCatalog(cust, c.getTitle(), c.getAuthor(), c.getType(), c.getGenre()));
+		assertEquals(false,library.addProductToCatalog(cust, g.getTitle(), g.getAuthor(), g.getType(), g.getGenre()));
+		assertEquals(false,library.addProductToCatalog(cust, d.getTitle(), d.getAuthor(), d.getType(), d.getGenre()));
+		assertEquals(false,library.addProductToCatalog(cust, m.getTitle(), m.getAuthor(), m.getType(), m.getGenre()));
 	}
 	
 	@Test
-	void testLibAddProductToCatalog() {
-		assertEquals(true,library.addProductToCatalog(cust, p.getTitle(), p.getAuthor(), p.getType(), p.getGenre()));
+	void testLibRemoveProductFromCatalog() {
+		library.addProductToCatalog(lib, b.getTitle(), b.getAuthor(), b.getType(), b.getGenre());
+		assertEquals(true,library.removeProductFromCatalog(lib, b.getTitle(), b.getAuthor(), b.getType(), b.getGenre()));
 	}
+	
+	@Test
+	void testCustRemoveProductFromCatalog() {
+		library.addProductToCatalog(lib, b.getTitle(), b.getAuthor(), b.getType(), b.getGenre());
+		assertEquals(false,library.removeProductFromCatalog(cust, b.getTitle(), b.getAuthor(), b.getType(), b.getGenre()));
+	}
+	
+	
 
 }
